6cea55e3c7900322d8e198e1f710dd6f2a22e771
Michael Shpilt <michaels@codevalue.net>
2019-09-27 15:56:59 +0300

    Read one file

---
+++ PracticalDebugingDemos/Demos/Files/ReadOneFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalDebugingDemos.Demos.Files
+{
+    public class ReadOneFile : DemoBase
+    {
+        public override void Start()
+        {
+            var files = Directory.GetFiles(@"C:\Windows");
+            ReadFirstFile(files);
+        }
+
+        private void ReadFirstFile(string[] files)
+        {
+            var firstFile = files.First();
+            ReadFile(firstFile);
+        }
+
+        private void ReadFile(string file)
+        {
+            var lines = File.ReadAllLines(file);
+        }
+    }
+}
