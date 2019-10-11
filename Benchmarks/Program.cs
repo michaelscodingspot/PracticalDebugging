551b47f4bc0ed5101db4095e1acfe7f129caf573
Michael Shpilt <michaels@codevalue.net>
2019-10-11 14:34:18 +0300

    Adding serialize benchmark for small class

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using BenchmarkDotNet.Running;
 using Benchmarks.Memory;
+using Benchmarks.Serializers;
 
 namespace Benchmarks
 {
@@ -9,7 +10,15 @@ namespace Benchmarks
         static void Main(string[] args)
         {
             //BenchmarkRunner.Run<StringBuilderFewStrings>();
-            BenchmarkRunner.Run<StringBuilderManyStrings>();
+            //BenchmarkRunner.Run<StringBuilderManyStrings>();
+            BenchmarkRunner.Run<SerializeCustomer>();
+            //var x = new SerializeCustomer();
+            //var s1 = x.SystemTextJson();
+            //var s2 = x.Newtonsoft();
+            //var s3 = x.DataContractJsonSerializer(); //with UTC
+            //var s4 = x.Jil();
+            //var s5 = x.Utf8Json(); //same output as newtonsoft 
+            //var s6 = x.ServiceStack();// with wrong UTC 0000
         }
     }
 }
---
+++ Benchmarks/Serializers/SerializeSmallClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text.Json.Serialization;
+using BenchmarkDotNet.Attributes;
+using Jil;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using SST = ServiceStack.Text;
+
+namespace Benchmarks.Serializers
+{
+    public class SerializeCustomer
+    {
+        [DataContract]  
+        class Customer
+        {
+            [DataMember]
+            public DateTime DateOfBirth { get; set; }
+            [DataMember]
+            public string FirstName { get; set; }
+            [DataMember]
+            public string LastName { get; set; }
+            [DataMember]
+            public int NumPurchases { get; set; }
+        }
+
+        private Customer _customer;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _customer = new Customer()
+            {
+                DateOfBirth = new DateTime(1955, 10, 28),
+                FirstName = "Bill",
+                LastName = "Gates",
+                NumPurchases = 1521
+            };
+        }
+
+        [Benchmark]
+        public string SystemTextJson()
+        {
+            return JsonSerializer.Serialize(_customer);
+        }
+
+        [Benchmark]
+        public string Newtonsoft()
+        {
+            return JsonConvert.SerializeObject(_customer);
+        }
+
+        [Benchmark]
+        public string DataContractJsonSerializer()
+        {
+            using (MemoryStream stream1 = new MemoryStream())
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Customer));
+                //TODO: Possibly change MemoryStream to StringWriter for performance
+                ser.WriteObject(stream1, _customer);
+                stream1.Position = 0;
+
+                // deserialize:
+                //var p2 = (Customer)ser.ReadObject(stream1);  
+
+                using var sr = new StreamReader(stream1);
+                return sr.ReadToEnd();
+            }
+        }
+
+        [Benchmark]
+        public string Jil()
+        {
+            using var output = new StringWriter();
+            JSON.Serialize(
+                _customer,
+                output
+            );
+            return output.ToString();
+        }
+
+        [Benchmark]
+        public string Utf8Json()
+        {
+            return JsonSerializer.Serialize(_customer); //.ToJsonString(_customer);
+        }
+
+        [Benchmark]
+        public string ServiceStack()
+        {
+            return SST.JsonSerializer.SerializeToString(_customer);
+        }
+
+        
+    }
+}
