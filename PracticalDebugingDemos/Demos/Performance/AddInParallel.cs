618cf13c66827d98a0412b774367fdb00dc923e7
Michael Shpilt <michaels9876@gmail.com>
2019-11-07 17:11:44 +0200

    Adding AddInParallel and ThreadsWaiting

---
+++ PracticalDebugingDemos/Demos/Performance/AddInParallel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalDebugingDemos.Demos.Performance
+{
+    class AddInParallel : DemoBase
+    {
+        private static object _lock = new object();
+
+        public override void Start()
+        {
+            int numbers = 500_500_000;
+            AppendTextToContent($"Adding {numbers} in parallel.");
+            Stopwatch sw = new Stopwatch();
+            int sum = 0;
+            Task.Run(() =>
+            {
+                //var numbersArr = CreateArray(numbers);
+                sw.Start();
+                sum = SumParallel(numbers); //SumSimple(numbersArr);// 
+                sw.Stop();
+            }).ContinueWith((t) =>
+            {
+                AppendTextToContent($"Finished in {sw.Elapsed.TotalSeconds} seconds. SumParallel = {sum}");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+
+        }
+
+        private static int[] CreateArray(int numbers)
+        {
+            return Enumerable.Range(0, numbers).Reverse().ToArray();
+        }
+
+        static int SumParallel(int items)
+        {
+            int total = 0;
+            int threads = Math.Min(items, 8);
+            var partSize = Math.Round(items / (double)threads + 0.5);
+            Task[] tasks = new Task[threads];
+            for (int iThread = 0; iThread < threads; iThread++)
+            {
+                var localThread = iThread;
+                tasks[localThread] = Task.Run(() =>
+                {
+                    var to = Math.Min(items, Math.Round((localThread + 1) * partSize + 0.5));
+                    for (int j = (int)(localThread * partSize); j < to; j++)
+                    {
+                        lock (_lock)
+                        {
+                            total += j;
+                        }
+                    }
+                });
+            }
+
+            Task.WaitAll(tasks);
+            //var verifySum = arr.SumParallel();
+            return total;
+        }
+
+        static int SumSimple(int items)
+        {
+            int sum = 0;
+            for (int i = 0; i < items; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ PracticalDebugingDemos/Demos/Performance/ThreadsWaiting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PracticalDebugingDemos.Demos.Performance
+{
+    class ThreadsWaiting : DemoBase
+    {
+        public override void Start()
+        {
+            object lock1 = new object();
+            AppendTextToContent("Started");
+
+            var task1 = Task.Run(() => { ThreadOperation(lock1, "Thread A"); });
+            var task2 = Task.Run(() => { ThreadOperation(lock1, "Thread B"); });
+            Task.WhenAll(task1, task2).ContinueWith((t) =>
+            {
+                AppendTextToContent("Finished");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private static void ThreadOperation(object lock1, string threadName)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (lock1)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{threadName} is running iteration {i}");
+                    Thread.Sleep(5000);
+                }
+            }
+
+
+        }
+    }
+}
