3d9d284cb63e47001ec78aabadf9a4de75cea3d2
Michael Shpilt <michaels9876@gmail.com>
2020-01-10 12:12:31 +0200

    Adding Calculator

---
+++ Calculator/CalculatorEngine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class CalculatorEngine
+    {
+        public CalculatorEngine()
+        {
+        }
+
+        internal int DoOperation(int operationId, int firstNumber, int secondNumber)
+        {
+            switch (operationId)
+            {
+                case 1:
+                    return Add(firstNumber, secondNumber);
+                case 2:
+                    return Substract(firstNumber, secondNumber);
+                case 3:
+                    return Multiply(firstNumber, secondNumber);
+                case 4:
+                    return Divide(firstNumber, secondNumber);
+                default:
+                    throw new InvalidOperationException($"The operation Id {operationId} is invalid.");
+
+            }
+        }
+
+        private int Add(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber + secondNumber;
+            return result;
+        }
+
+        private int Substract(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber - secondNumber;
+            return result;
+        }
+
+        private int Multiply(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber * secondNumber;
+            return result;
+        }
+
+        private int Divide(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber / secondNumber;
+            return result;
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetOperationItems(out int operationId, out int firstNumber, out int secondNumber);
+            var calc = new CalculatorEngine();
+            double result = calc.DoOperation(operationId, firstNumber, secondNumber);
+            Console.WriteLine("The result is: " + result);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static void GetOperationItems(out int operationId, out int firstNumber, out int secondNumber)
+        {
+            Console.WriteLine("Enter operation:");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Substract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+            operationId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter first number");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            secondNumber = int.Parse(Console.ReadLine());
+        }
+    }
+}
