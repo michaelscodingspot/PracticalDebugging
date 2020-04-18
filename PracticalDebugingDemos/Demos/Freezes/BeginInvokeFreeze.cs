0bd4cab032b5723bfff8a06cff51d5e3f820c488
Michael Shpilt <michaels9876@gmail.com>
2020-04-18 14:13:27 +0300

    BeginInvokeFreeze

---
+++ PracticalDebugingDemos/Demos/Freezes/BeginInvokeFreeze.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace PracticalDebuggingDemos.Demos.Freezes
+{
+    public class BeginInvokeFreeze : DemoBase
+    {
+        public override void Start()
+        {
+            A();
+        }
+
+private void A()
+{
+    Dispatcher.CurrentDispatcher.BeginInvoke((Action)(() => B()));
+}
+
+private void B()
+{
+    Dispatcher.CurrentDispatcher.BeginInvoke((Action)(() => A()));
+}
+    }
+}
