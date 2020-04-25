3c0600686469881a43361b9ce2dfff3d4de0731a
Michael Shpilt <michaels9876@gmail.com>
2020-04-25 14:46:38 +0300

    ManyAllocationsGCPressureGenXPressure

---
+++ PracticalDebugingDemos/Demos/Memory/ManyAllocationsGCPressureGen0Pressure.cs
@@ -0,0 +1,40 @@
+﻿using PracticalDebuggingDemos;
+using PracticalDebuggingDemos.Demos.Debug3rdPartyCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PracticalDebugingDemos.Demos.Memory
+{
+    public class ManyAllocationsGCPressureGen0Pressure : DemoBase
+    {
+        /// <summary>
+        /// In this test, there's constant allocations of short-lived objects on 5 threads.
+        /// Even though there's non-stop allocations, GC pressure isn't created. 
+        /// You'll see the performance counter "% Time in GC" rise to be around a fixed range - maybe 10 to 20%. 
+        /// Decreasing the threads to 1 thread will reduce it even more.
+        /// </summary>
+        public override void Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        var person = new Person()
+                        {
+                            Age = 1242,
+                            Name = "Bill Gates"
+                        };
+                    }
+                });
+            }
+            
+
+        }
+    }
+}
---
+++ PracticalDebugingDemos/Demos/Memory/ManyAllocationsGCPressureGen1Pressure.cs
@@ -0,0 +1,43 @@
+﻿using PracticalDebuggingDemos;
+using PracticalDebuggingDemos.Demos.Debug3rdPartyCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PracticalDebugingDemos.Demos.Memory
+{
+    public class ManyAllocationsGCPressureGen1Pressure : DemoBase
+    {
+        static List<Person> listStatic;
+
+        public override void Start()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    List<Person> list = new List<Person>();
+                    for (int i = 0; i < 12000; i++)
+                    {
+
+                        var person = new Person()
+                        {
+                            Age = 1242,
+                            Name = "Bill Gates"
+                        };
+                        list.Add(person);
+                    }
+                    Task.Run(() =>
+                    {
+                        Thread.Sleep(1000);
+                        var list2 = list;
+                        listStatic = list2;
+                    });
+                }
+            });
+        }
+    }
+}
